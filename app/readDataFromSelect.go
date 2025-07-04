201b0b86b64712ebfcc3bd2e06d2753efa381139
spaghetti-lover <phungducanh2511@gmail.com>
2025-07-04 23:06:15 +0700

    codecrafters submit [skip ci]

--- app/readDataFromSelect.go
+++ app/readDataFromSelect.go
@@ -68,6 +68,20 @@ func readDataFromSelect(databaseFilePath, tableName string, colNames []string, w
 			break
 		}
 	}
+
+	// Trong readDataFromSelect, sau khi lấy rootpage của bảng, hãy tìm thêm rootpage của index:
+	var indexRootpage int
+	for _, offset := range cellArray {
+		rec, _ := parseRecord(page, int(offset))
+		if len(rec.Values) < 5 {
+			continue
+		}
+		if rec.Values[0] == "index" && rec.Values[1] == "idx_companies_country" && rec.Values[2] == "companies" {
+			indexRootpage, _ = strconv.Atoi(strings.TrimSpace(rec.Values[3]))
+			break
+		}
+	}
+
 	if rootpage == 0 || createSQL == "" {
 		return nil, fmt.Errorf("table %s not found in database", tableName)
 	}
@@ -183,4 +197,139 @@ func scanTableBTree(db *os.File, pageSize uint16, pageNum int, colIdxs []int, wh
 		}
 	}
 	return results, nil
+}
+
+func scanIndexForRowids(db *os.File, pageSize uint16, pageNum int, whereVal string) ([]int64, error) {
+	offset := int64((pageNum - 1) * int(pageSize))
+	page := make([]byte, pageSize)
+
+	if _, err := db.Seek(offset, io.SeekStart); err != nil {
+		return nil, err
+	}
+	if _, err := db.Read(page); err != nil {
+		return nil, err
+	}
+
+	pageType := page[0]
+	results := []int64{}
+
+	switch pageType {
+	case 10: // Leaf index b-tree page
+		dataPageHeader := parsePageHeader(bytes.NewReader(page))
+		for _, cellPtr := range dataPageHeader.CellPointers {
+			// Parse cell: [payload_size][key][rowid]
+			pos := int(cellPtr)
+			_, n1 := readVarint(page[pos:]) // payload_size
+			pos += n1
+
+			// Parse record header size (varint)
+			headerSize, n2 := readVarint(page[pos:])
+			pos += n2
+
+			// Parse serial types
+			serialTypes := []int{}
+			headerBytesRead := n2
+			for headerBytesRead < int(headerSize) {
+				serial, n := readVarint(page[pos:])
+				serialTypes = append(serialTypes, int(serial))
+				pos += n
+				headerBytesRead += n
+			}
+			// Parse values (key columns)
+			values := []string{}
+			bodyPos := pos
+			for _, st := range serialTypes {
+				val, size := readValueBySerialType(page[bodyPos:], st)
+				values = append(values, val)
+				bodyPos += size
+			}
+			// So sánh country (giả sử index chỉ có 1 cột là country)
+			if len(values) > 0 && strings.EqualFold(strings.TrimSpace(values[0]), strings.TrimSpace(whereVal)) {
+				// Sau key là rowid (varint)
+				rowid, _ := readVarint(page[bodyPos:])
+				results = append(results, int64(rowid))
+			}
+		}
+	case 2: // Interior index b-tree page
+		dataPageHeader := parsePageHeader(bytes.NewReader(page))
+		for _, cellPtr := range dataPageHeader.CellPointers {
+			if int(cellPtr)+4 > len(page) {
+				continue
+			}
+			childPageNum := int(binary.BigEndian.Uint32(page[cellPtr : cellPtr+4]))
+			childResults, err := scanIndexForRowids(db, pageSize, childPageNum, whereVal)
+			if err == nil {
+				results = append(results, childResults...)
+			}
+		}
+		// right-most pointer
+		if len(page) >= 12 {
+			rightMostPtr := int(binary.BigEndian.Uint32(page[8:12]))
+			if rightMostPtr > 0 {
+				childResults, err := scanIndexForRowids(db, pageSize, rightMostPtr, whereVal)
+				if err == nil {
+					results = append(results, childResults...)
+				}
+			}
+		}
+	}
+	return results, nil
+}
+
+func getRecordByRowid(db *os.File, pageSize uint16, pageNum int, rowid int64) (Record, error) {
+	offset := int64((pageNum - 1) * int(pageSize))
+	page := make([]byte, pageSize)
+
+	if _, err := db.Seek(offset, io.SeekStart); err != nil {
+		return Record{}, err
+	}
+	if _, err := db.Read(page); err != nil {
+		return Record{}, err
+	}
+
+	pageType := page[0]
+
+	switch pageType {
+	case 13: // Leaf table b-tree page
+		dataPageHeader := parsePageHeader(bytes.NewReader(page))
+		for _, cellPtr := range dataPageHeader.CellPointers {
+			foundRowid, rec, err := parseRecordWithRowid(page, int(cellPtr))
+			if err != nil {
+				continue
+			}
+			if int64(foundRowid) == rowid {
+				return rec, nil
+			}
+		}
+		return Record{}, fmt.Errorf("rowid %d not found in leaf page %d", rowid, pageNum)
+	case 5: // Interior table b-tree page
+		dataPageHeader := parsePageHeader(bytes.NewReader(page))
+		// Duyệt các cell để tìm child page chứa rowid
+		for i, cellPtr := range dataPageHeader.CellPointers {
+			// Mỗi cell: [child_page (4 bytes)][key_rowid (varint)]
+			pos := int(cellPtr)
+			if pos+4 > len(page) {
+				continue
+			}
+			childPageNum := int(binary.BigEndian.Uint32(page[pos : pos+4]))
+			// Đọc key_rowid (varint) sau 4 bytes
+			keyRowid, n := readVarint(page[pos+4:])
+			// Nếu rowid < key_rowid thì duyệt child này
+			if rowid < int64(keyRowid) {
+				return getRecordByRowid(db, pageSize, childPageNum, rowid)
+			}
+			// Nếu là cell cuối cùng, duyệt tiếp
+			if i == len(dataPageHeader.CellPointers)-1 {
+				// right-most pointer
+				if len(page) >= 12 {
+					rightMostPtr := int(binary.BigEndian.Uint32(page[8:12]))
+					return getRecordByRowid(db, pageSize, rightMostPtr, rowid)
+				}
+			}
+		}
+		// Nếu không tìm thấy, trả lỗi
+		return Record{}, fmt.Errorf("rowid %d not found in interior page %d", rowid, pageNum)
+	default:
+		return Record{}, fmt.Errorf("unsupported page type %d", pageType)
+	}
 }
